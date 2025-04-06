8f9787575bd557c4841415ae8e807bbac5970b64
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-04-06 22:57:16 +0300

    Переделана функция добавления пользователя + добавлена функция проверки пользователя

--- SRC/DataBase/Function_User_DataBase.py
+++ SRC/DataBase/Function_User_DataBase.py
@@ -2,9 +2,16 @@ from SRC.DataBase.DataBase import Session
 from SRC.Models.User import User
 
 
-def add_user():
+def add_user(id: int, name: str):#Добавление нового пользователя
     session = Session()
-    new_user = User(telegram_id=1, user_name = "Test")
-    session.add(new_user)
-    session.commit()
+    new_user = User(telegram_id=id, user_name = name)
+    if check_user(id):
+        session.add(new_user)
+        session.commit()
     session.close()
+
+def check_user(id: int):#Проверка пользователя на нахождение в бд
+    session = Session()
+    if session.query(User).filter_by(telegram_id=id).first() == None:
+        return True
+    return False
