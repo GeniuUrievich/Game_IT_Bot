ddf52e0b8d6036059279f0f473d20ce0f0ea0e59
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-04-07 23:50:46 +0300

    Создана функция добавления

---
+++ SRC/DataBase/Function_Task.py
@@ -0,0 +1,21 @@
+from SRC.DataBase.DataBase import Session
+from SRC.DataBase.Function_Level import get_level
+from SRC.DataBase.Function_Programming_language import get_language
+from SRC.Models.Task import Task
+
+async def add_task(condition: str, answer:str, level_title:str, prog_language:str):
+    level = await get_level(level_title)
+    language = await get_language(prog_language)
+    if not level and not language:
+        print("Ошибка")
+        return
+
+    new_task = Task(
+        condition=condition,
+        answer=answer,
+        level_id=level.id,
+        programming_language_id=language.id
+    )
+    async with Session() as session:
+        async with session.begin():
+            session.add(new_task)
