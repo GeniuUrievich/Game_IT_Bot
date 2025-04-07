a66dd4758a7a868b9edae67142e4b6c357ee35b7
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-04-07 22:13:27 +0300

    Создана функция добавления уровня

---
+++ SRC/DataBase/Function_Level.py
@@ -0,0 +1,14 @@
+from SRC.DataBase.DataBase import Session
+from SRC.Models.Level import Level
+
+Levels = [
+    Level(title="Новичок", description="Только начал изучение. Осваиваешь основы — переменные, типы данных, простые команды."),
+    Level(title="Ученик", description="Уже понимаешь, как писать простые программы. Работаешь с циклами, условиями, функциями."),
+    Level(title="Продвинутый", description="Пишешь собственные проекты, знаком с ООП, базами данных и асинхронностью."),
+    Level(title="Разработчик", description="Уверенно проектируешь архитектуру, работаешь с фреймворками, знаешь, как строить приложения."),
+    Level(title="Мастер", description="Не просто пишешь код — ты создаёшь системы. Настраиваешь автоматизацию, обучаешь других и ведёшь проекты")
+]
+async def add_level():
+    async with Session() as session:
+        async with session.begin():
+            session.add_all(Levels)
