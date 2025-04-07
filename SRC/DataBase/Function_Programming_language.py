c146506f8d5d469a5f81426a6e2da3eee59b1d29
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-04-07 23:49:54 +0300

    Добавлена функция для получения языка

--- SRC/DataBase/Function_Programming_language.py
+++ SRC/DataBase/Function_Programming_language.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from SRC.DataBase.DataBase import Session
 from SRC.Models.Programming_language import Programming_language
 
@@ -11,3 +13,9 @@ async def add_language():
     async with Session() as session:
         async with session.begin():
             session.add_all(Languages)
+async def get_language(language:str):
+    async with Session() as session:
+        async with session.begin():
+            query = select(Programming_language).filter_by(language=language)
+            result = await session.execute(query)
+            return result.scalars().first()
