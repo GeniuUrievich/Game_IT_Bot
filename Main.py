565e1fc60ad101249b3e611d416a77f5b0471b6b
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-04-20 21:48:17 +0300

    Summa

--- Main.py
+++ Main.py
@@ -1,13 +1,17 @@
+import asyncio
 import os
 import sys
 
-from SRC.DataBase.Function_User_DataBase import add_user
+from SRC.DataBase.Function_Achievement import add_achievement, add_achiv_user  # , issuie_achiv_user
+from SRC.DataBase.Function_Level import add_level
+from SRC.DataBase.Function_Programming_language import add_language, get_language
+from SRC.DataBase.Function_Task import add_task, check_answer
+from SRC.DataBase.Function_User_DataBase import add_user, true_answer_task, false_answer_task, issue_task
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 from SRC.DataBase.DataBase import create_table, drop_table
 
-
-#drop_table()
-create_table()
-add_user(3, "Pavel")
+async def main():
+    await add_achiv_user(1, "Первый шаг")
+asyncio.run(main())
