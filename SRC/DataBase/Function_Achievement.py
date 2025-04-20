8f73f88ed950939026783025603bcc97d3eb4684
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-04-20 21:48:36 +0300

    Переписаны функции, но пока не рабочие

--- SRC/DataBase/Function_Achievement.py
+++ SRC/DataBase/Function_Achievement.py
@@ -1,10 +1,11 @@
 from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 
 from SRC.DataBase.DataBase import Session
-from SRC.Models import User
+from SRC.Models.User import User
 from SRC.Models.Achievements import Achievement
 
-async def add_achievenent(title_s:str, description_s:str, requirements_s:str):
+async def add_achievement(title_s:str, description_s:str, requirements_s:str):
     async with Session() as session:
         new_achievement = Achievement(title=title_s, description=description_s, requirements=requirements_s)
         async with session.begin():
@@ -21,15 +22,13 @@ async def check_achievement(title_ach: str):#Проверка достижени
                 return True
     return False
 
-async def check_ach_user(telegr_id:int, title_ach: str):
+async def add_achiv_user(id:int, title_ac:str):
     async with Session() as session:
         async with session.begin():
-            query1 = select(User).filter_by(teleg_id=telegr_id)
-            query2 = select(Achievement).filter_by(title=title_ach)
-            result1 = await session.execute(query1)
-            result2 = await session.execute(query2)
+            result1 = await session.execute(select(User).options(joinedload(User.achievements)).filter_by(telegram_id=id))
             user = result1.scalars().first()
+            result2 = await session.execute(select(Achievement).filter_by(title=title_ac))
             achievement = result2.scalars().first()
-            if achievement not in user.achievements:
-                return True
-    return False
+            if achievement not in user.achievements and user.task_all_done == int(achievement.requirements):
+                user.achievements.append(achievement)
+                await session.commit()
