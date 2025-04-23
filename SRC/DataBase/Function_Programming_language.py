6c45bf7512ded7a9ec83d81a9f9559545f83ca93
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-04-24 00:14:41 +0300

    Написана функция получения всех языков

--- SRC/DataBase/Function_Programming_language.py
+++ SRC/DataBase/Function_Programming_language.py
@@ -1,5 +1,4 @@
 from sqlalchemy import select
-
 from SRC.DataBase.DataBase import Session
 from SRC.Models.Programming_language import Programming_language
 
@@ -19,3 +18,10 @@ async def get_language(language:str):
             query = select(Programming_language).filter_by(language=language)
             result = await session.execute(query)
             return result.scalars().first()
+
+async def get_all_language():
+    async with Session() as session:
+        async with session.begin():
+            query = select(Programming_language.language)
+            result = await session.execute(query)
+            return result.scalars().all()
